1f6da54bd199233e5d1d0dd764a233121d0dfbcd
Bo Xu <i@186526.xyz>
2026-04-23 13:38:09 +0000

    在构建云镜像工作流中添加构建时间和发布名称，优化 zs-guest-tools 安装脚本

--- scripts/install-zs-guest-tools.sh
+++ scripts/install-zs-guest-tools.sh
@@ -4,7 +4,8 @@ set -e
 # 解压并安装 zs-guest-tools.tar.gz
 cd /tmp
 if [ -f /tmp/zs-guest-tools.tar.gz ]; then
-    tar -xzf zs-guest-tools.tar.gz
-    cd zs-guest-tools || exit 1
+    mkdir -p /tmp/zs-guest-tools
+    tar -xzf zs-guest-tools.tar.gz -C /tmp/zs-guest-tools
+    cd /tmp/zs-guest-tools || exit 1
     bash ./zs-tools-install.sh || ./zs-tools-install.sh || true
 fi
